03b8584b7f4cfab58eca877c4543dbad5bd710d8
exxxcellent <aleksejdikun@mail.ru>
2024-06-12 21:50:15 +0300

    initial

---
+++ README.md
@@ -0,0 +1,3 @@
+# Dodo Pizza Copy
+
+This React application is copy Dodo Pizza website
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <>
+            dodo
+        </>
+    )
+}
+
+export default App
